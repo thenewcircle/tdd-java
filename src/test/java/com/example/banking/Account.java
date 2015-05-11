1674e8be1a50906436eb56a8a5fe7a8602972c90
Jacob D. Parr <me@jacobparr.com>
2015-05-10 21:55:14 -0700

    Added test to assert account balance.

---
+++ src/test/java/com/example/banking/Account.java
@@ -0,0 +1,16 @@
+package com.example.banking;
+
+public class Account {
+
+  private double balance;
+
+  public Account(double balance) {
+    super();
+    this.balance = balance;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+}
--- src/test/java/com/example/banking/BankingServiceTest.java
+++ src/test/java/com/example/banking/BankingServiceTest.java
@@ -14,7 +14,16 @@ public class BankingServiceTest {
   public void testTransfer() {
 
     long fromAccountId = 1L;
+    double fromBalance = 1_000;
+    
     long toAccountId = 1L;
+    double toBalance = 5;
+    
+    Account fromAccount = new Account(1_000);
+    Account toAccount = new Account(5);
+
+    Assert.assertEquals(fromBalance, fromAccount.getBalance(), 0.00_001);
+    Assert.assertEquals(toBalance, toAccount.getBalance(), 0.00_001);
     
     BankingService teller = new SimpleBankingService();
 
