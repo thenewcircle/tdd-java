281a4426d5cc069e5af65c1c45add58794da9153
Jacob D. Parr <me@jacobparr.com>
2015-05-10 20:16:46 -0700

    Added Banking Service Test

---
+++ src/test/java/com/example/banking/BankingServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.banking;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankingServiceTest {
+
+  @Test
+  public void testHelloBanking() {
+    Assert.assertTrue(true);
+  }
+  
+}
