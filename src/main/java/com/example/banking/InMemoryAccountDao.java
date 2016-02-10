81bf8f9b2cb3486094ba96de380b6b3fa240f4e3
Jacob D. Parr <me@jacobparr.com>
2016-02-10 15:46:15 -0500

    Post refactor

--- src/main/java/com/example/banking/InMemoryAccountDao.java
+++ src/main/java/com/example/banking/InMemoryAccountDao.java
@@ -1,5 +1,8 @@
 package com.example.banking;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InMemoryAccountDao implements AccountDao {
 
   private static final AccountDao DAO = new InMemoryAccountDao();
@@ -8,42 +11,27 @@ public class InMemoryAccountDao implements AccountDao {
     return DAO;
   }
 
-//  private final Map<Long,Account> accounts = new HashMap<>();
-  
-  private Account accountA = new Account(1L, "Tom", 1_100);
-  private Account accountB = new Account(2L, "Dick", 5);
+  private final Map<Long,Account> accounts = new HashMap<>();
   
   private InMemoryAccountDao() {
+    accounts.put(1L, new Account(1L, "Tom", 1_100));
+    accounts.put(2L, new Account(2L, "Dick", 5));
   }
   
   @Override
   public Account getAccount(long id) throws AccountNotFoundException {
-    if (id == 1L) {
-      // accountA = new Account(id, "Tom", 1_100);
-      return accountA;
-
-    } else if (id == 2L){
-      // accountB = new Account(id, "Dick", 5);
-      return accountB;
-
-    } else {
+    if (accounts.containsKey(id) == false) {
       throw new AccountNotFoundException(id);
     }
+    return accounts.get(id);
   }
 
   @Override
   public Account saveAccount(Account account) throws AccountNotFoundException {
-    if (1L == account.getAccountId()) {
-      accountA = account;
-      return account;
-
-    } else if (2L == account.getAccountId()) {
-      accountB = account;
-      return account;
-    
-    } else {
+    if (accounts.containsKey(account.getAccountId()) == false) {
       throw new AccountNotFoundException(account.getAccountId());
     }
+    return accounts.put(account.getAccountId(), account);
   }
 }
 
